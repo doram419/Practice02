833291c7db327b2d86a753bdf88f042cb6f608d4
worm0 <worm0@112.187.177.32>
2024-03-15 20:36:35 +0900

    24.03.15

--- src/com/javaex/practice/Ex14.java
+++ src/com/javaex/practice/Ex14.java
@@ -14,36 +14,33 @@ public class Ex14 {
 		
 //		사번(양의정수)을 입력
 //		(사번 0은 고려하지 않습니다.)
-		System.out.print("사번(정수)를 입력하세요");
+		System.out.print("사번(정수)를 입력하세요\n사번 : ");
 		int employeeNum = sc.nextInt();
 		int quo = employeeNum % 3;
 		
 		// 팀 구분
-//		사번을 3으로 나누어서 나머지가 0이면 A팀
-		
-		if(employeeNum > 0)
+		// 사번을 3으로 나누어서 나머지가 0이면 A팀
+		if(employeeNum >= 0)
 		{
+			// 기본을 A팀으로 둠 
+			String team = "A팀";
 			
+			// 나머지가 1이면 B팀
+			if(quo == 1)
+			{
+				team = "B팀";
+			}
+			// 나머지가 2이면 C팀
+			else if(quo == 2)
+			{
+				team = "C팀";
+			}
+			// 팀 출력
+			System.out.println(team + "입니다.");
 		}
 		else {
 			System.out.println("잘못 입력하셨습니다.");
 		}
-		// 기본을 A팀으로 둠 
-		String team = "A팀";
-		
-		// 나머지가 1이면 B팀
-		if(quo == 1)
-		{
-			team = "B팀";
-		}
-		// 나머지가 2이면 C팀
-		else if(quo == 2)
-		{
-			team = "C팀";
-		}
-
-// 		팀 출력
-		System.out.println(" ");
 		
 		// 스캐너 닫기
 		sc.close();
